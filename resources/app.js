d39c275ec5ee93945fd0bd746e0b08752d05e6c3
[siddhart meena] <[siddgothwal369@gmail.com]>
2021-02-02 02:35:03 +0530

    text animation added

--- resources/app.js
+++ resources/app.js
@@ -1,5 +1,33 @@
+// for loading 
 let loadClass = document.querySelector('.loading');
 
 window.addEventListener('load',function(){
     loadClass.parentElement.removeChild(loadClass);
 });
+
+// for text animation
+
+let lastTitle = document.querySelector('.web');
+let val = lastTitle.textContent;
+let arr = val.split("");
+lastTitle.textContent ="";
+arr.forEach(item=>{
+    lastTitle.innerHTML += "<span>"+item+"</span>"
+})
+let char =0;
+let timer = setInterval(onTick,50);
+function onTick(){
+    const span = document.querySelectorAll('.content span')[char];
+    span.classList.add('fade');
+    char++;
+    if(char ===arr.length){
+        complete();
+        return;
+    }
+}
+function complete(){
+    clearInterval(timer);
+    timer = null;
+}
+
+// for scroll effect
